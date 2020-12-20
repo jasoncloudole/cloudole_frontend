9df2ce078f8a06bf170bfb0fbeb29cb7d7aeca5a
jason <jason@thecloudole.com>
2020-12-20 16:43:39 -0500

    remove commented lines

--- src/_components/PublicRoute.js
+++ src/_components/PublicRoute.js
@@ -11,8 +11,6 @@ const isLogin = () => {
 }
 const PublicRoute = ({ component: Component, restricted, ...rest }) => {
     return (
-        // restricted = false meaning public route
-        // restricted = true meaning restricted route
         <Route {...rest} render={props => (
             isLogin() && restricted ?
                 <Redirect to="/" />
